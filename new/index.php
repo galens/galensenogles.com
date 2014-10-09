70d89f597bb9154515a4ee8091802e1c25a750df
galens <galen.senogles@gmail.com>
2014-10-09 09:35:34 -0700

    adding updated site folder

---
+++ new/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li class="logo"></li>
+          <li><a href="#">File</a></li>
+          <li><a href="#">File</a></li>
+          <li><a href="#">Edit </a></li>
+          <li><a href="#">View</a></li>
+          <li><a href="#">Label</a></li>
+          <li><a href="#">Special</a></li>
+        </ul>
+        <ul>
+          <li class="clock"><time>4:20 PM</time></li>
+          <li class="about"></li>
+          <li class="winselector"></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <section aria-labelledby="about_this">
+        <h2 id="about_this">About this website</h2>
+      </section>
+      <section aria-labelledby="about_this">
+        <h2 id="about_this">About this website</h2>
+      </section>
+    </main>
+  </body>
+</html>
