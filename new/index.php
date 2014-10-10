168d565a43189215c62663816afbc8342a0245cb
galens <galen.senogles@gmail.com>
2014-10-10 08:55:54 -0700

    menu addition

--- new/index.php
+++ new/index.php
@@ -13,18 +13,72 @@
     <header>
       <nav>
         <ul>
-          <li class="logo"></li>
-          <li><a href="#">File</a></li>
-          <li><a href="#">File</a></li>
-          <li><a href="#">Edit </a></li>
-          <li><a href="#">View</a></li>
-          <li><a href="#">Label</a></li>
-          <li><a href="#">Special</a></li>
+          <li class="logo"><a href="#">Logo</a></li>
+          <li><a href="#">File</a>
+            <ul>
+              <li><a href="#">New Folder</a></li>
+              <li><a href="#">Open</a></li>
+              <li><a href="#">Print</a></li>
+              <li><a href="#">Close Window</a></li>
+              <li><a href="#">Find</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Edit</a>
+            <ul>
+              <li><a href="#">Undo</a></li>
+              <li><a href="#">Cut</a></li>
+              <li><a href="#">Copy</a></li>
+              <li><a href="#">Paste</a></li>
+              <li><a href="#">Clear</a></li>
+              <li><a href="#">Select All</a></li>
+            </ul>
+          </li>
+          <li><a href="#">View</a>
+            <ul>
+              <li><a href="#">by Small Icon</a></li>
+              <li><a href="#">by Icon</a></li>
+              <li><a href="#">by Name</a></li>
+              <li><a href="#">by Size</a></li>
+              <li><a href="#">by Kind</a></li>
+              <li><a href="#">by Label</a></li>
+              <li><a href="#">by Date</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Label</a>
+            <ul>
+              <li><a href="#">None</a></li>
+              <li><a href="#">Essential</a></li>
+              <li><a href="#">Hot</a></li>
+              <li><a href="#">In Progress</a></li>
+              <li><a href="#">Cool</a></li>
+              <li><a href="#">Personal</a></li>
+              <li><a href="#">Project 1</a></li>
+              <li><a href="#">Project 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Special</a>
+            <ul>
+              <li><a href="#">Clean up Window</a></li>
+              <li><a href="#">Empty Trash...</a></li>
+              <li><a href="#">Eject Disk</a></li>
+              <li><a href="#">Erase Disk</a></li>
+              <li><a href="#">Restart</a></li>
+              <li><a href="#">Shutdown</a></li>
+            </ul>
+          </li>
         </ul>
         <ul>
-          <li class="clock"><time>4:20 PM</time></li>
-          <li class="about"></li>
-          <li class="winselector"></li>
+          <li class="clock"><time><a href="#">4:20 PM</a></time></li>
+          <li class="about"><a href="#">?</a>
+            <ul>
+              <li><a href="#">Show Balloons</a></li>
+            </ul>
+          </li>
+          <li class="winselector"><a href="#">Sel</a>
+            <ul>
+              <li><a href="#">Finder</a></li>
+            </ul>
+          </li>
         </ul>
       </nav>
     </header>
